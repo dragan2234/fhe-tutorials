c50081a0e9a6222ad70e564e9fab40f0ec809dc3
Dragan Pilipovic <draganpilipovic@Dragans-MacBook-Pro.local>
2023-04-21 03:52:17 +0200

    input message bit padding done

--- tfhe/examples/sha256/src/main.rs
+++ tfhe/examples/sha256/src/main.rs
@@ -2,9 +2,21 @@ use tfhe::{ConfigBuilder, generate_keys, set_server_key, FheUint32};
 use tfhe::prelude::*;
 
 // H constants
-// const H: [u32; 8] = [
-//     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
-// ];
+const H: [u32; 8] = [
+    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
+];
+
+// K constants
+const K: [u32; 64] = [
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+];
 
 // struct HCipers {
 //     inner: GenericInteger<P>
@@ -34,44 +46,59 @@ fn main() {
     use std::time::Instant;
     let now = Instant::now();
     {
-    let config = ConfigBuilder::all_disabled()
-        .enable_default_uint32()
-        .build();
+    // let config = ConfigBuilder::all_disabled()
+    //     .enable_default_uint32()
+    //     .build();
 
     // Client-side
-    let (client_key, server_key) = generate_keys(config);
+    // let (client_key, server_key) = generate_keys(config);
 
-    let clear_a = 27u32;
-    let clear_b = 128u32;
+    // let clear_a = 27u32;
+    // let clear_b = 128u32;
 
-    let a = FheUint32::try_encrypt(clear_a, &client_key).unwrap();
-    let b = FheUint32::try_encrypt(clear_b, &client_key).unwrap();
+    // let a = FheUint32::try_encrypt(clear_a, &client_key).unwrap();
+    // let b = FheUint32::try_encrypt(clear_b, &client_key).unwrap();
 
     //Server-side
-    set_server_key(server_key);
-    let result = a + b;
+    // set_server_key(server_key);
+    // let result = a + b;
 
 
     //Client-side
-    let decrypted_result: u32 = result.decrypt(&client_key);
+    // let decrypted_result: u32 = result.decrypt(&client_key);
 
-    let clear_result = clear_a + clear_b;
+    // let clear_result = clear_a + clear_b;
 
+    let input_message = "hello world";
 
-    let result = string_to_u32_vector("hello world");
+    let bit_length = input_message.as_bytes().len() * 8;
+    println!("{:?}", bit_length);
 
-    // let mut kurac = string_to_u32_vector("hello world");
+    let mut result = string_to_u32_vector(input_message);
 
-    let a = InputCiphertext::encrypt(result, &client_key);
+    for i in result.len()..16 {
+        result.push(0u32);
+    }
+    println!("{:?}", result);
 
-    let res = a.inner.first().unwrap();
+    let mut eight_bits = split_into_8bit_vector(&result.clone());
 
-    let decr: u32 = res.decrypt(&client_key);
-    // assert_eq!(decrypted_result, clear_result);
-    // let final_ = kurac.first().unwrap();
+    println!("{:?}", eight_bits);
 
-    // let inal = final_.clone();
-    // assert_eq!(inal, 104u32);
+    replace_first_zero_byte(&mut eight_bits);
+
+    println!("{:?}", eight_bits);
+
+    let returned = vec_u8_to_u32(&eight_bits);
+
+    println!("{:?}", returned);
+
+    // let a = InputCiphertext::encrypt(result, &client_key);
+
+    // let res = a.inner.first().unwrap();
+
+    // let decr: u32 = res.decrypt(&client_key);
+
     }
 
     let elapsed = now.elapsed();
@@ -95,22 +122,40 @@ fn string_to_u32_vector(s: &str) -> Vec<u32> {
 
     result
 }
-// H constants
-// const H: [u32; 8] = [
-//     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
-// ];
 
-// // K constants
-// const K: [u32; 64] = [
-//     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-//     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-//     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-//     0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-//     0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-//     0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-//     0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-//     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
-// ];
+fn split_into_8bit_vector(vec: &Vec<u32>) -> Vec<u8> {
+    let mut result = Vec::new();
+    for value in vec.iter() {
+        for i in 0..4 {
+            result.push(((value >> (24 - i * 8)) & 0xFF) as u8);
+        }
+    }
+    result
+}
+
+fn replace_first_zero_byte(vec: &mut Vec<u8>) {
+    for byte in vec.iter_mut() {
+        if *byte == 0 {
+            *byte = 0b10000000;
+            break;
+        }
+    }
+}
+
+fn vec_u8_to_u32(input: &[u8]) -> Vec<u32> {
+    assert_eq!(input.len() % 4, 0, "Input length must be a multiple of 4");
+    let mut output = Vec::with_capacity(input.len() / 4);
+    for i in (0..input.len()).step_by(4) {
+        let byte_slice = &input[i..i + 4];
+        let mut bytes = [0u8; 4];
+        bytes[..byte_slice.len()].copy_from_slice(byte_slice);
+        let value = u32::from_be_bytes(bytes);
+        output.push(value);
+    }
+    output
+}
+
+
 
 // pub struct Sha256 {
 //     state: [u32; 8],
